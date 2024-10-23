fe17cde458343a0236ba45023edefbcd5272d663
Marlon Macedo <marlonsantosmacedo@outlook.com>
2024-10-23 00:49:55 -0300

    feat: make status bar react to theme changes

--- composeApp/src/androidMain/kotlin/com/mxrsoon/volare/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/mxrsoon/volare/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.LaunchedEffect
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent { App() }
+
+        setContent {
+            LaunchedEffect(isSystemInDarkTheme()) {
+                enableEdgeToEdge()
+            }
+
+            App()
+        }
     }
 }
