fa1fdbd66f51ac5684f191fabae2f8cb9c5ccd3f
Marlon Macedo <marlonsantosmacedo@outlook.com>
2024-10-23 01:00:25 -0300

    feat: add top app bar to collections page

--- composeApp/src/commonMain/kotlin/com/mxrsoon/volare/collections/CollectionsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/mxrsoon/volare/collections/CollectionsScreen.kt
@@ -11,27 +11,44 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.mxrsoon.volare.common.ui.padding.plus
 import com.mxrsoon.volare.composeapp.generated.resources.Res
+import com.mxrsoon.volare.composeapp.generated.resources.collections_label
 import com.mxrsoon.volare.composeapp.generated.resources.item_count_format
 import com.mxrsoon.volare.composeapp.generated.resources.item_list_label
 import com.mxrsoon.volare.composeapp.generated.resources.task_list_label
 import org.jetbrains.compose.resources.stringResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CollectionsScreen() {
-    Scaffold(Modifier.imePadding()) { innerPadding ->
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    Scaffold(
+        modifier = Modifier.imePadding(),
+        topBar = {
+            MediumTopAppBar(
+                title = { Text(text = stringResource(Res.string.collections_label)) },
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
         val contentPadding = innerPadding + 24.dp
 
         LazyVerticalStaggeredGrid(
             modifier = Modifier
+                .nestedScroll(scrollBehavior.nestedScrollConnection)
                 .consumeWindowInsets(contentPadding)
                 .fillMaxSize(),
             columns = StaggeredGridCells.Adaptive(200.dp),
