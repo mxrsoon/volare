75e3c044232dc4b10e7f1e312b668449160ac0e1
Marlon Macedo <marlonsantosmacedo@outlook.com>
2024-10-23 00:49:34 -0300

    feat: improve login keyboard handling

--- composeApp/src/commonMain/kotlin/com/mxrsoon/volare/common/ui/dialog/Keyboard.kt
+++ composeApp/src/commonMain/kotlin/com/mxrsoon/volare/common/ui/dialog/Keyboard.kt
@@ -4,14 +4,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
 
 /**
  * Intercepts the enter key press and triggers the provided action.
  */
 fun Modifier.onEnterKeyDown(action: () -> Unit): Modifier =
-    this.onKeyEvent { event ->
+    this.onPreviewKeyEvent { event ->
         if (event.key.keyCode == Key.Enter.keyCode && event.type == KeyEventType.KeyDown) {
             action()
             true
