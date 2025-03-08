b81b798f2f0c61cfbdb2cf2c9f21dc792e66f0cf
Marlon Macedo <marlonsantosmacedo@outlook.com>
2025-03-08 17:23:56 -0300

    refactor: remove unused date format method

--- composeApp/src/iosMain/kotlin/com/mxrsoon/volare/common/datetime/DateTimeFormatHelper.ios.kt
+++ composeApp/src/iosMain/kotlin/com/mxrsoon/volare/common/datetime/DateTimeFormatHelper.ios.kt
@@ -1,13 +1,10 @@
 package com.mxrsoon.volare.common.datetime
 
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
-import kotlinx.datetime.atStartOfDayIn
 import kotlinx.datetime.toInstant
 import kotlinx.datetime.toNSDate
 import platform.Foundation.NSDateFormatter
-import platform.Foundation.NSDateFormatterNoStyle
 import platform.Foundation.NSDateFormatterShortStyle
 import platform.Foundation.NSLocale
 import platform.Foundation.autoupdatingCurrentLocale
@@ -16,20 +13,6 @@ import platform.Foundation.autoupdatingCurrentLocale
  * Helper for formatting dates and times.
  */
 actual object DateTimeFormatHelper {
-
-    /**
-     * Format the date in a human-readable format, using the preferred format for the user's locale.
-     */
-    actual fun format(date: LocalDate): String {
-        val platformDate = date.atStartOfDayIn(TimeZone.currentSystemDefault()).toNSDate()
-        val formatter = NSDateFormatter()
-
-        formatter.dateStyle = NSDateFormatterShortStyle
-        formatter.timeStyle = NSDateFormatterNoStyle
-        formatter.locale = NSLocale.autoupdatingCurrentLocale
-
-        return formatter.stringFromDate(platformDate)
-    }
 
     /**
      * Format the date and time in a human-readable format, using the preferred format for the user's locale.
--- composeApp/src/jvmAndAndroidMain/kotlin/com/mxrsoon/volare/common/datetime/DateTimeFormatHelper.jvmAndAndroid.kt
+++ composeApp/src/jvmAndAndroidMain/kotlin/com/mxrsoon/volare/common/datetime/DateTimeFormatHelper.jvmAndAndroid.kt
@@ -2,23 +2,13 @@ package com.mxrsoon.volare.common.datetime
 
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.toJavaLocalDate
 import kotlinx.datetime.toJavaLocalDateTime
 
 /**
  * Helper for formatting dates and times.
  */
 actual object DateTimeFormatHelper {
-
-    /**
-     * Format the date in a human-readable format, using the preferred format for the user's locale.
-     */
-    actual fun format(date: LocalDate): String =
-        DateTimeFormatter
-            .ofLocalizedDate(FormatStyle.SHORT)
-            .format(date.toJavaLocalDate())
 
     /**
      * Format the date and time in a human-readable format, using the preferred format for the user's locale.
