c7e17dd5e9347b5b7b876ea1ab7409c5f675492b
Marlon Macedo <marlonsantosmacedo@outlook.com>
2025-03-08 17:16:44 -0300

    fix(datetime): fix date time formatting on iOS

--- composeApp/src/iosMain/kotlin/com/mxrsoon/volare/common/datetime/DateTimeFormatHelper.ios.kt
+++ composeApp/src/iosMain/kotlin/com/mxrsoon/volare/common/datetime/DateTimeFormatHelper.ios.kt
@@ -2,14 +2,15 @@ package com.mxrsoon.volare.common.datetime
 
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.toNSDateComponents
-import platform.Foundation.NSCalendarUnitDay
-import platform.Foundation.NSCalendarUnitHour
-import platform.Foundation.NSCalendarUnitMinute
-import platform.Foundation.NSCalendarUnitMonth
-import platform.Foundation.NSCalendarUnitYear
-import platform.Foundation.NSDateComponentsFormatter
-import platform.Foundation.NSFormattingUnitStyleShort
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toNSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSDateFormatterNoStyle
+import platform.Foundation.NSDateFormatterShortStyle
+import platform.Foundation.NSLocale
+import platform.Foundation.autoupdatingCurrentLocale
 
 /**
  * Helper for formatting dates and times.
@@ -20,29 +21,27 @@ actual object DateTimeFormatHelper {
      * Format the date in a human-readable format, using the preferred format for the user's locale.
      */
     actual fun format(date: LocalDate): String {
-        val dateComponents = date.toNSDateComponents()
-        val formatter = NSDateComponentsFormatter()
+        val platformDate = date.atStartOfDayIn(TimeZone.currentSystemDefault()).toNSDate()
+        val formatter = NSDateFormatter()
 
-        formatter.allowedUnits = NSCalendarUnitDay or NSCalendarUnitMonth or NSCalendarUnitYear
-        formatter.unitsStyle = NSFormattingUnitStyleShort
+        formatter.dateStyle = NSDateFormatterShortStyle
+        formatter.timeStyle = NSDateFormatterNoStyle
+        formatter.locale = NSLocale.autoupdatingCurrentLocale
 
-        return formatter.stringFromDateComponents(dateComponents)
-            ?: throw IllegalStateException("Failed to format date")
+        return formatter.stringFromDate(platformDate)
     }
 
     /**
      * Format the date and time in a human-readable format, using the preferred format for the user's locale.
      */
     actual fun format(dateTime: LocalDateTime): String {
-        val dateComponents = dateTime.toNSDateComponents()
-        val formatter = NSDateComponentsFormatter()
+        val platformDate = dateTime.toInstant(TimeZone.currentSystemDefault()).toNSDate()
+        val formatter = NSDateFormatter()
 
-        formatter.allowedUnits = NSCalendarUnitDay or NSCalendarUnitMonth or NSCalendarUnitYear or
-                NSCalendarUnitHour or NSCalendarUnitMinute
+        formatter.dateStyle = NSDateFormatterShortStyle
+        formatter.timeStyle = NSDateFormatterShortStyle
+        formatter.locale = NSLocale.autoupdatingCurrentLocale
 
-        formatter.unitsStyle = NSFormattingUnitStyleShort
-
-        return formatter.stringFromDateComponents(dateComponents)
-            ?: throw IllegalStateException("Failed to format date and time")
+        return formatter.stringFromDate(platformDate)
     }
 }
