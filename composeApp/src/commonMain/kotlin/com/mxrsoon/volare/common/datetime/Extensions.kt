f98040d6bc02ed18a5b2220429d6cb20bd94c409
Marlon Macedo <marlonsantosmacedo@outlook.com>
2025-03-08 15:56:07 -0300

    refactor(datetime): temporarily remove unused extension

--- composeApp/src/commonMain/kotlin/com/mxrsoon/volare/common/datetime/Extensions.kt
+++ composeApp/src/commonMain/kotlin/com/mxrsoon/volare/common/datetime/Extensions.kt
@@ -1,14 +1,8 @@
 package com.mxrsoon.volare.common.datetime
 
-import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 
 /**
  * Format the date and time in a human-readable format, using the preferred format for the user's locale.
  */
 fun LocalDateTime.format(): String = DateTimeFormatHelper.format(this)
-
-/**
- * Format the date in a human-readable format, using the preferred format for the user's locale.
- */
-fun LocalDate.format(): String = DateTimeFormatHelper.format(this)
